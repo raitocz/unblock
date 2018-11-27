f03270575e2bc4583663bcde67d6eca8a8e5e9c1
Raito Akehanareru <raito.akehanareru@gmail.com>
2018-11-27 07:43:29 +0100

    Init commit

---
+++ Assets/Scripts/ChangeSelfColor.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeSelfColor : MonoBehaviour
+{
+
+    public float colorDuration = 100;
+
+    public Gradient gradient;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float t = Mathf.Repeat(Time.time, colorDuration) / colorDuration;
+
+        this.GetComponent<SpriteRenderer>().color = gradient.Evaluate(t);
+
+        if (this.GetComponent<ParticleSystem>() != null)
+        {
+            ParticleSystem ps = GetComponent<ParticleSystem>();
+            ps.startColor = gradient.Evaluate(t);
+        }
+    }
+}
